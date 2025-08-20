b7ddd17441bde1c1aaa34b41be22c362c31235d1
seoguhoe2 <js8337349@naver.com>
2025-08-20 15:16:39 +0900

    Pokemon 필드 작성

---
+++ battlefactory/pokemon/PokemonSpecies.java
@@ -0,0 +1,13 @@
+package battlefactory.pokemon;
+
+import battlefactory.pokemon.aggregate.Type;
+
+import java.util.Map;
+
+public class PokemonSpecies {
+    private int number;
+    private String name;
+    private Type type;
+    private Map<String, Integer> speciesStat;
+    private String ability;
+}
